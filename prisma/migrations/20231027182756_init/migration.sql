c6977aa38b0a91ed8535a8299b805aca3c4b7f5c
raoufchebri <raouf@chebri.com>
2023-10-27 11:34:48 -0700

    add-symbol

---
+++ prisma/migrations/20231027182756_init/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "atomic_element" (
+    "atomic_number" INTEGER NOT NULL,
+    "element_name" TEXT,
+
+    CONSTRAINT "atomic_element_pkey" PRIMARY KEY ("atomic_number")
+);
---
+++ prisma/migrations/20231027183212_symbol/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "atomic_element" ADD COLUMN     "symbol" TEXT;
